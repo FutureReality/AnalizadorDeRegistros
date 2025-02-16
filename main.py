3114c7599a944c6eb4096b28c9ee16857246f3d1
FutureReality <115637305+FutureReality@users.noreply.github.com>
2025-02-16 22:57:45 +0100

    Me confundi y no lo agregue en el anterior commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from funciones import informe
+
+while True:
+    print("Bienvenido al lector de log's")
+    ruta = input("Diga la ruta de su fichero: ") #En windows funciona raro
+    informe(ruta)
