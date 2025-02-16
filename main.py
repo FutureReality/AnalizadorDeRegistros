71d73ef13bac38088f907705fabf0f47e4be46e8
FutureReality <115637305+FutureReality@users.noreply.github.com>
2025-02-16 23:11:30 +0100

    Agregado un try except

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from funciones import informe
 while True:
     print("Bienvenido al lector de log's")
     ruta = input("Diga la ruta de su fichero: ") #En windows funciona raro
-    informe(ruta)
+    try:
+        informe(ruta)
+    except FileNotFoundError:
+        print(f"Error: No se encontro fichero en la ruta '{ruta}'")
+    except Exception as e:
+        print(f"Se produjo un error desconocido: {e}")
